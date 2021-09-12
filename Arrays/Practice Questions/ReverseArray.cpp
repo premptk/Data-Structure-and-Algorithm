7ac6864d2e4c218ba83a7439e54e95a7f3ec7ad7
Prem Ranjan Pattanayak <premranjan210@gmail.com>
2021-09-12 17:06:34 +0530

    #1

---
+++ Arrays/Practice Questions/ReverseArray.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int arr[]={1,2,3,4,5};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int temp;
+    for(int i=0;i<n/2;i++)
+    {
+        temp = arr[i];
+        arr[i] = arr[n-i-1];
+        arr[n-i-1] = temp;
+    }
+    for(int i=0;i<n;i++)
+        cout<<arr[i]<<" ";
+    return 0;
+}
