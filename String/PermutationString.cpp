6d9ba0cc657de958a1b3d77fad40d2754a4a805c
Prem Ranjan Pattanayak <premranjan210@gmail.com>
2021-09-28 19:24:30 +0530

    prints permutation of a given string

---
+++ String/PermutationString.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void permutation(string str, int l, int r)
+{
+    if(l==r)
+    {
+        cout<<str<<endl;
+        return;
+    }
+    for(int i=l;i<=r;i++)
+    {
+        swap(str[i], str[l]);
+        permutation(str,l+1,r);
+        swap(str[i],str[l]);
+    }
+}
+
+int main()
+{
+    string str = "ABC";
+    permutation(str,0,str.length()-1);
+    return 0;
+}
