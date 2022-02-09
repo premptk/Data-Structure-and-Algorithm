07e3f245ccf85442c7c37a56492d32d86dcbb73d
Prem Ranjan Pattanayak <premranjan210@gmail.com>
2022-02-09 19:17:42 +0530

    Ceil and Floor in a BST

--- Tree/Binary Search Tree/Ceil_Floor_BST.cpp
+++ Tree/Binary Search Tree/Ceil_Floor_BST.cpp
@@ -57,6 +57,25 @@ int findCeil(TreeNode* root, int key)
     return ceil;
 }
 
+int findFloor(TreeNode* root, int key)
+{
+    int ceil = -1;
+    while(root)
+    {
+        if(root->data==key){
+            ceil = root->data;
+            return ceil;
+        }
+        else if(root->data < key){
+            ceil = root->data;
+            root = root->right;
+        }
+        else
+            root=root->left;
+    }
+    return ceil;
+}
+
 int main()
 {
     TreeNode* root=NULL;
@@ -69,5 +88,6 @@ int main()
     PrintTree(root);
     cout<<"\n";
     cout<<findCeil(root, 8)<<endl;
+    cout<<findFloor(root, 21)<<endl;
     return 0;
 }
