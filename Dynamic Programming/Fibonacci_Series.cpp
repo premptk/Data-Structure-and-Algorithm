fe4fbfc136deaf0119c3434e9f04011b12bea944
Prem Ranjan Pattanayak <premranjan210@gmail.com>
2022-02-18 00:35:01 +0530

    1. Fibonacci series

--- Dynamic Programming/Fibonacci_Series.cpp
+++ Dynamic Programming/Fibonacci_Series.cpp
@@ -1,26 +1,45 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+// Memoization
 int fibo(int n, vector<int> &dp)
 {
     if(n<=1)
         return n;
-  
-    //checking for overlapping subproblem
-    if(dp[n]!=-1)
+    if(dp[n]!=-1) 
         return dp[n];
-  
-    // return and initializing dp
     return dp[n] = fibo(n-1, dp) + fibo(n-2, dp);
+}
+
+// Tabulation
+int fibo2(int n, vector<int> dp)
+{
+    dp[0]=0;
+    dp[1]=1;
+    for(int i=2;i<=n;i++)
+        dp[i] = dp[i-1] + dp[i-2];
+    return dp[n];
+}
+
+// Space optimization Approach
+int fibo3(int n)
+{
+    int prev2=0,prev=1;
+    for(int i=2;i<=n;i++)
+    {
+        int curi = prev + prev2;
+        prev2=prev;
+        prev=curi;
+    }
+    return prev;
 }
 
 int main()
 {
     int n=10;
-  
-    // step 1 - declaration
     vector<int> dp(n+1, -1);
-  
-    cout<<fibo(n, dp);
+    cout<<fibo2(n,dp)<<endl;
+    cout<<fibo(n, dp)<<endl;
+    cout<<fibo3(n);
     return 0;
 }
