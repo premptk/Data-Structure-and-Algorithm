b31ce28086dd4640805d074d5f2e3a0460cc44d7
Prem Ranjan Pattanayak <premranjan210@gmail.com>
2022-07-02 23:58:32 +0530

    DP 43. Longest Increasing Subsequence | Binary Search

--- Dynamic Programming/LIS_BinarySearch.cpp
+++ Dynamic Programming/LIS_BinarySearch.cpp
@@ -8,7 +8,7 @@ int LIS(vector<int> &arr, int n)
     temp.push_back(arr[0]);
     for(int i=1;i<n;i++)
     {
-        if(temp[i] < arr[i])
+        if(temp.back() < arr[i])
         {
             temp.push_back(arr[i]);
             len++;
