90745352f2e7fb5f1cc537ac6e4aac30b6be54a0
Prem Ranjan Pattanayak <premranjan210@gmail.com>
2022-07-19 23:11:57 +0530

    Check if two trees are same or not?

--- Tree/Binary Tree/SameTreeORnot.cpp
+++ Tree/Binary Tree/SameTreeORnot.cpp
@@ -17,7 +17,7 @@ bool isSame(TreeNode* root1, TreeNode* root2)
     if(root1==NULL || root2==NULL)
         return(root1==root2);
     return root1->data==root2->data 
-    && isSame(root1->left, root2->right) 
+    && isSame(root1->left, root2->left) 
     && isSame(root1->right, root2->right);
 }
 
